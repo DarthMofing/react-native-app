4f6e129924d8bc7fef332a75cbdc12b5c6b0d7c2
Saul Uriel <sauluriel1402@gmail.com>
2021-07-01 00:49:55 -0600

    delete function, edit badge screen, and search bar

--- src/components/BadgesScreen/BadgesScreen.js
+++ src/components/BadgesScreen/BadgesScreen.js
@@ -1,66 +1,140 @@
 import React from 'react'
-import { View, ActivityIndicator, StyleSheet, Text, FlatList } from 'react-native'
+import { View, ActivityIndicator, StyleSheet, Text, FlatList, Alert, StatusBar } from 'react-native'
 import Colors from '../../res/Colors'
 import Http from '../../libs/http'
 import BadgesItem from './BadgesItem'
+import BadgesSearch from './BadgesSearch'
 
 class BadgesScreen extends React.Component {
-    state = {
-        loading:false,
-        badges:[
+  state = {
+    loading: false,
+    badges: undefined,
+    badgesCopy: undefined,
+  }
+  componentDidMount() {
+    this.fetchdata()
+    this.focusEvent()
+    this.blurEvent()
+  }
 
-        ],
-    }
-    componentDidMount(){
-        this.fetchdata()
-    }
-    fetchdata = async() => {
-        this.setState({loading:true})
-        let response = await Http.instance.get_all()
-        response = response.reverse()
-        console.log(response)
-        this.setState({loading:false,badges:response})
-    }
+  focusEvent = () => {
+    this.focusListener = this.props.navigation.addListener('focus', () => {
+      this.setFetchInterval()
+    })
+  }
 
-    handlePress = item =>{
-        this.props.navigation.navigate('BadgesDetails', {item})
-    }
+  blurEvent = () => {
+    this.blurListener = this.props.navigation.addListener('blur', () => {
+      clearInterval(this.interval)
+    })
+  }
 
-    render(){
-        const{badges,loading} = this.state
-        return(
-            <View style={[styles.container,styles.horizontal]}>
-                {loading ? (
-                    <ActivityIndicator style={styles.loader} color="#0C77F2" size="large"/>
-                ) : null}
-                <FlatList 
-                    style={styles.list} 
-                    data={badges} 
-                    renderItem={({item}) => <BadgesItem key={item._id} item={item} onPress={()=>this.handlePress(item)}/>}
-                />
-            </View>
-        )
+  setFetchInterval = () =>{
+    this.interval = setInterval(this.fetchdata,3000)
+  }
+
+  fetchdata = async () => {
+    this.setState({ loading: true })
+    let response = await Http.instance.get_all()
+    this.setState({ loading: false, badges: response, badgesCopy: response })
+  }
+
+  handlePress = item => {
+    this.props.navigation.navigate('BadgesDetails', { item })
+  }
+
+  handleEdit = item => {
+    this.props.navigation.navigate('BadgesEdit',{item})
+  }
+
+  handleChange = query => {
+    const {badgesCopy} = this.state
+    const badgesFiltered = badgesCopy.filter(badge => {
+      return badge.name.toLowerCase().includes(query.toLowerCase())
+    })
+    this.setState({badges:badgesFiltered})
+    if(query){
+      clearInterval(this.interval)
+    }else{
+      this.setFetchInterval()
     }
+  }
+
+  handleDelete = item => {
+    Alert.alert('Are you sure?',
+      `Do you want to delete ${item.name}'s badge?\n\nThis process cannot be reverted.`,
+      [
+        {
+          text:'Cancel',
+          style:'cancel',
+        },
+        {
+          text:'Delete',
+          onPress: async () =>{
+            this.setState({loading:true,badges:undefined})
+            await Http.instance.remove(item._id)
+            this.fetchdata() 
+          },
+          style:'destructive',
+        }
+      ],
+      {
+        cancelable:true,
+      }
+    )
+  }
+
+  componentWillUnmount() {
+    this.focusListener()
+    this.blurListener()
+  }
+
+  render() {
+    const { badges, loading } = this.state
+    if(loading===true && !badges){
+      return(
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator style={styles.loader} color="#0C77F2" size="large" />
+        </View>
+      )
+    }
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <StatusBar backgroundColor="transparent" translucent={true}/>
+        <BadgesSearch onChange={this.handleChange}/>
+        <FlatList
+          style={styles.list}
+          data={badges}
+          renderItem={({ item }) => (
+            <BadgesItem key={item._id} item={item} onPress={() => this.handlePress(item)}
+              onEdit={() => this.handleEdit(item)}
+              onDelete={() => this.handleDelete(item)} />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        flexDirection:'column',
-        backgroundColor:Colors.charade,
-    },
-    horizontal:{
-        justifyContent:'space-around',
-        alignItems:'center',
-    },
-    loader:{
-        height:'100%',
-        paddingHorizontal:10,
-    },
-    list:{
-        width:'100%',
-        paddingHorizontal:10,
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: Colors.charade,
+  },
+  horizontal: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  loader: {
+    height: '100%',
+    paddingHorizontal: 10,
+  },
+  list: {
+    width: '100%',
+    paddingHorizontal: 10,
+  },
 })
 
 export default BadgesScreen
--- src/components/BadgesScreen/BadgesStack.js
+++ src/components/BadgesScreen/BadgesStack.js
@@ -4,25 +4,27 @@ import BadgesLanding from '../BadgesLanding/BadgesLanding'
 import Colors from '../../res/Colors'
 import BadgesScreen from './BadgesScreen'
 import BadgesDetails from '../BadgesDetails/BadgesDetails'
+import BadgesEdit from '../BadgesEdit/BadgesEdit'
 
 const Stack = createStackNavigator()
 
-const BadgesStack = () =>{
-    return(
-        <Stack.Navigator
-            screenOptions={{
-                headerStyle:{
-                    backgroundColor:Colors.blackPearl,
-                    shadowColor:Colors.blackPearl,
-                },
-                headerTintColor:Colors.white,
-            }}
-        >
-            <Stack.Screen name="Landing" component={BadgesLanding} options={{ headerShown:false}}/>
-            <Stack.Screen name="Badges" component={BadgesScreen}/>
-            <Stack.Screen name="BadgesDetails" component={BadgesDetails}/>
-        </Stack.Navigator>
-    )
+const BadgesStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.blackPearl,
+          shadowColor: Colors.blackPearl,
+        },
+        headerTintColor: Colors.white,
+      }}
+    >
+      <Stack.Screen name="Landing" component={BadgesLanding} options={{ headerShown: false }} />
+      <Stack.Screen name="Badges" component={BadgesScreen} />
+      <Stack.Screen name="BadgesDetails" component={BadgesDetails} />
+      <Stack.Screen name="BadgesEdit" component={BadgesEdit}/>
+    </Stack.Navigator>
+  )
 }
 
 export default BadgesStack
